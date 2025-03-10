64f589432095149ac9504a95389173a41015598b
Krish <krishjatav246@gmail.com>
2025-03-10 19:02:22 +0530

    update

--- src/pages/homePages/Home.jsx
+++ src/pages/homePages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from './hero'
+import Hero from './Hero'
 import Mission from './Mission'
 import Specialisations from './Specialisations'
 import Commitment from './Commitment'
