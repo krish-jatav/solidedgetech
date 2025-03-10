e854ed7fdb107d419f4b502f3e8316badae638d1
Krish <krishjatav246@gmail.com>
2025-03-10 19:04:09 +0530

    update

--- src/pages/homePages/Home.jsx
+++ src/pages/homePages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Hero from './Hero'
 import Mission from './Mission'
 import Specialisations from './Specialisations'
 import Commitment from './Commitment'
 import Discover from './Discover'
+import Hero from './hero'
 
 const Home = () => {
   return (
